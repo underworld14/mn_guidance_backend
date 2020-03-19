2edfe53097fdaee306947ec1e4b0c4c2689659f8
zaulsr <zaulsr14@gmail.com>
2020-03-19 14:48:25 +0700

    add student seeder

---
+++ src/db/seeders/20200319063958-demo-student.js
@@ -0,0 +1,35 @@
+"use strict";
+const fs = require("fs");
+
+const read = JSON.parse(fs.readFileSync(`${__dirname}/../source/student.json`, "utf-8"));
+
+let data = read.map(val => {
+  return {
+    ...val,
+    birthdate: val.birthdate ? new Date(val.birthdate) : null,
+    created_at: new Date(),
+    updated_at: new Date()
+  };
+});
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkInsert("students", data, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
