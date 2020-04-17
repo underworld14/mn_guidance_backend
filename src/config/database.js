75541b12ccfaf0b16faf34bd916dfe5fbeb63a37
zaulsr <zaulsr14@gmail.com>
2020-04-18 06:43:54 +0700

    add prod config

--- src/config/database.js
+++ src/config/database.js
@@ -21,6 +21,6 @@ module.exports = {
     password: "hf99d580e4a6a6b072cf0b7f6bbed8fc7b1fd1d9282554f9612297e11755cf2f7",
     database: "mdfuu199ko2agdi",
     host: "ec2-23-20-129-146.compute-1.amazonaws.com",
-    dialect: "mysql",
+    dialect: "postgres",
   },
 };
